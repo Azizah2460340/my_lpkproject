10bd721b71077e53cc3033177f7a6745c52d4b7b
Azizah2460340 <zahputzah.08@gmail.com>
2025-07-12 09:10:26 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,9 @@ st.markdown("*Streamlit* is **really** ***cool***.")
  
 if st.button("Aloha", type="tertiary"):
     st.write("Ciao")
+import streamlit as st
+
+sentiment_mapping = ["one", "two", "three", "four", "five"]
+selected = st.feedback("stars")
+if selected is not None:
+    st.markdown(f"You selected {sentiment_mapping[selected]} star(s).")
