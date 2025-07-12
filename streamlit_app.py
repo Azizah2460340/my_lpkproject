312066364414d75dc666dd56e27cd3626ba15ec0
Azizah2460340 <zahputzah.08@gmail.com>
2025-07-12 08:48:09 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 
+st.write("Hello, *World!* :sunglasses:")
+
 st.title("🎈 My new app")
 st.write(
+
+st.markdown("*Streamlit* is **really** ***cool***.")
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
